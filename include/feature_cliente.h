de691b75735e3d9f2005f0cc31d32b0410fa9a3d
Mari-Silva20 <mariane.silva@aluno.ueg.br>
2025-11-15 12:14:49 -0300

    Add client management function declarations

--- include/feature_cliente.h
+++ include/feature_cliente.h
@@ -1,1 +1,23 @@
-sfbsdbjb\
+#ifndef CLIENTE_H
+#define CLIENTE_H
+#include <stdio.h>
+
+int cadastrarCliente(char clientes[][20], int *numClientes, const char codigo[]);
+
+int validarCPF(const char *cpf);
+
+int validarCNPJ(const char *cnpj);
+
+int validarTelefone(const char *telefone);
+
+int validarEmail(const char *email);
+
+int salvarCliente(const char *nome, const char *endereco, const char *telefone, const char *cpf, const char *cnpj, const char *tipo, const char *email);
+
+int consultarCliente(const char clientes[][20], int numClientes, const char codigo[]);
+
+int removerCliente(char clientes[][20], int *numClientes, const char codigo[]);
+
+void listarClientes(const char clientes[][20], int numClientes);
+
+#endif // CLIENTE_H
