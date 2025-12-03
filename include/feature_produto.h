4be762a37d2263c0cf57bdceb9ee075ba0710637
João Paulo da Costa <joao.costa.854@aluno.ueg.br>
2025-12-03 18:02:30 -0300

    Enhance feature_produto.h with struct and guards
    Added header guards and defined Produto struct with related functions.

--- include/feature_produto.h
+++ include/feature_produto.h
@@ -1,6 +1,30 @@
-int apagar_produto();
-int codigoExiste ();
-int novo_produto();
-void limpar_buffer();
-int modulo_produto();
-int modulo_novo_produto ();
+#ifndef FEATURE_PRODUTO_H
+#define FEATURE_PRODUTO_H
+
+// Definicao da struct de   Produto
+typedef struct {
+    int id;
+    char descricao[100];
+    double preco;
+    int estoque;
+} Produto;
+
+//..................................................................
+
+// Funções de Persistência
+void carregarProdutosCSV();
+void salvarProdutosCSV();
+
+//...................................................................
+
+// Funcoes de interface 
+
+void cadastrarProduto();
+void consultarProduto();
+void removerProdutoInterface();
+void listarProdutos();
+
+
+//int buscarProdutoPorId(int id); (modulo - pedido - kauan)
+
+#endif
