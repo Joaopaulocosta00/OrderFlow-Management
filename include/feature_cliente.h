b89bffd39c0705cc2dcf18b18a1d6c50dad55a02
João Paulo da Costa <joao.costa.854@aluno.ueg.br>
2025-12-03 18:02:45 -0300

    Refactor cliente header for feature_cliente

--- include/feature_cliente.h
+++ include/feature_cliente.h
@@ -1,28 +1,23 @@
-#ifndef CLIENTE_H
-#define CLIENTE_H
+#ifndef FEATURE_CLIENTE_H
+#define FEATURE_CLIENTE_H
 
-int cadastrarCliente(struct Cliente clientes[], int *numClientes, struct Cliente novo);
+typedef struct {
+    char codigo[20];
+    char nome[50];
+    char endereco[100];
+    char telefone[15];
+    char documento[20];
+    char tipo;
+    char email[50];
+} Cliente;
 
-int validarCodigo(const char *codigo);
+void carregarClientesCSV();
+void salvarClientesCSV();
 
-int validarCPF(const char *cpf);
+// Funco0es de Interface
+void cadastrarCliente();
+void listarClientes();
+void removerClienteInterface();
+void consultarCliente(); 
 
-int validarCNPJ(const char *cnpj);
-
-int verificarTelefone(const char *telefone) ;
-
-int validarEmail(const char *email);
-
-int salvarCliente(const char *codigo, const char *nome, const char *endereco, const char *telefone, const char *cpf, const char *cnpj, const char *tipo, const char *email);
-
-int consultarCliente(struct Cliente clientes[], int numClientes, const char codigo[]);
-
-int removerCliente(struct Cliente clientes[], int *numClientes, const char codigo[]);
-
-void listarClientes(struct Cliente clientes[], int numClientes);
-
-void carregarClientesDoArquivo();
-
-int codigoPrincipal();
-
-#endif // CLIENTE_H
+#endif
